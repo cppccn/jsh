a481a233fdb3ebfb0ae686d15cb25c25f3800714
Yvan Sraka <yvan@sraka.xyz>
2021-05-19 20:11:50 +0000

    Update README using @phaazon cargo sync-readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# jsh
+<!-- cargo-sync-readme start -->
+
+# `jsh`
+
+_A sandboxed shell that input and output JSON (which is handy)_
+
+`jsh` intends to be secure by running everything inside a `chroot` and use a whitelist of authorized programs.
+
+Built in the first place as a dependency of `pndr`, it should be able to be used through SSH!
+
+<!-- cargo-sync-readme end -->
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+//! # `jsh`
+//! 
+//! _A sandboxed shell that input and output JSON (which is handy)_
+//! 
+//! `jsh` intends to be secure by running everything inside a `chroot` and use a whitelist of authorized programs.
+//! 
+//! Built in the first place as a dependency of `pndr`, it should be able to be used through SSH!
+
 use std::path::Path;
 
 pub fn exec(_path: &Path, _cmd: &str) {
